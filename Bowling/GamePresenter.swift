2309165d25230ee49adc95a85f936588d8289aa8
Víctor Jiménez <vjimenezg@viewnext.com>
2020-08-18 17:30:31 +0200

    Test empty game

---
+++ Bowling/GamePresenter.swift
@@ -0,0 +1,18 @@
+//
+//  GamePresenter.swift
+//  Bowling
+//
+//  Created by Víctor Jiménez on 18/08/2020.
+//  Copyright © 2020 Víctor Jiménez. All rights reserved.
+//
+
+import Foundation
+
+class GamePresenter {
+    
+    var score = 0
+    
+    func frame(pins: Int) {
+        
+    }
+}
